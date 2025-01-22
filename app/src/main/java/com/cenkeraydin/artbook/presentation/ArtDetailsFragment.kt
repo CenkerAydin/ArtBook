053b778e2c9c9d80bccad7de0a494c520329347f
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2025-01-22 12:36:26 +0300

    add fragments and layouts

---
+++ app/src/main/java/com/cenkeraydin/artbook/presentation/ArtDetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.cenkeraydin.artbook.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cenkeraydin.artbook.R
+
+class ArtDetailsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_art_details, container, false)
+    }
+
+}

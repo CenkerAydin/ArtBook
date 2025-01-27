1bdc6141c7bf16e9f6735c115b87d1dc7442ea3a
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2025-01-27 15:57:42 +0300

    add repository and glide module implementation

--- app/src/main/java/com/cenkeraydin/artbook/di/AppModule.kt
+++ app/src/main/java/com/cenkeraydin/artbook/di/AppModule.kt
@@ -2,8 +2,14 @@ package com.cenkeraydin.artbook.di
 
 import android.content.Context
 import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.cenkeraydin.artbook.R
+import com.cenkeraydin.artbook.data.repository.ArtRepository
+import com.cenkeraydin.artbook.data.repository.ArtRepositoryImpl
 import com.cenkeraydin.artbook.data.retrofit.RetrofitAPI
 import com.cenkeraydin.artbook.data.room.ArtDB
+import com.cenkeraydin.artbook.data.room.ArtDao
 import com.cenkeraydin.artbook.util.Util.BASE_URL
 import dagger.Module
 import dagger.Provides
@@ -41,4 +47,15 @@ object AppModule {
             .build()
             .create(RetrofitAPI::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideRepo(dao: ArtDao, api: RetrofitAPI) = ArtRepositoryImpl(dao, api) as ArtRepository
+
+    @Singleton
+    @Provides
+    fun glideModule(@ApplicationContext context: Context) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+            .error(R.drawable.ic_launcher_foreground)
+    )
 }

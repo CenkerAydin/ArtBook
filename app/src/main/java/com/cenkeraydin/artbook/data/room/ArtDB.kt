46d5c4a78df525e6084e09ef8fa21993ca41a285
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2025-01-28 12:30:14 +0300

    set to exportScheme false

--- app/src/main/java/com/cenkeraydin/artbook/data/room/ArtDB.kt
+++ app/src/main/java/com/cenkeraydin/artbook/data/room/ArtDB.kt
@@ -3,7 +3,7 @@ package com.cenkeraydin.artbook.data.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Art::class], version =1)
+@Database(entities = [Art::class], version =1, exportSchema = false)
 abstract class ArtDB : RoomDatabase() {
     abstract fun artDao(): ArtDao
 }

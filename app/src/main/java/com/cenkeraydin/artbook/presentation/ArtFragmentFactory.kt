8703dcc5fb05736e6b002467c2b1c078267593c8
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2025-02-01 16:28:53 +0300

    change factory name

--- app/src/main/java/com/cenkeraydin/artbook/presentation/ArtFragmentFactory.kt
+++ app/src/main/java/com/cenkeraydin/artbook/presentation/ArtFragmentFactory.kt
@@ -2,13 +2,11 @@ package com.cenkeraydin.artbook.presentation
 
 import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
-import com.cenkeraydin.artbook.data.room.Art
 import com.cenkeraydin.artbook.presentation.adapter.ArtRecyclerAdapter
 import com.cenkeraydin.artbook.presentation.adapter.ImageRecyclerAdapter
-import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-class FragmentFactory @Inject constructor(
+class ArtFragmentFactory @Inject constructor(
     private val artRecyclerAdapter: ArtRecyclerAdapter,
     private val glide: RequestManager,
     private val imageRecyclerAdapter: ImageRecyclerAdapter
